20c86fda6028469408878d65aad228b2e227570b
Beatriz Miotto <miotto.beatriz2008@gmail.com>
2025-04-25 11:24:06 -0300

    ex02

---
+++ listaarray/ex02.js
@@ -0,0 +1,6 @@
+let playlist = ["a ultima dança", "That Should Be Me", "One Time", "Love Yourself"];
+let quantidade = playlist.length;
+
+for (let i = 0; i < quantidade; i++) {
+    console.log(playlist[i]);
+}
