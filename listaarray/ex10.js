9ed75b6d89abcc4d919ab30b133dbf1e5f95e8f2
biamiotto <miotto.beatriz2008@gmail.com>
2025-04-26 13:30:03 -0300

    ex10

---
+++ listaarray/ex10.js
@@ -0,0 +1,8 @@
+let jogos = ["Among us", "Roblox", "Valorant"];
+let jogofav = "Roblox"
+
+for (let i = 0; i < jogos.length; i++) {
+    if (jogofav == jogos[i]) {
+        console.log(`O meu jogo favorito é ${jogos[i]} e está na posição ${i + 1}`);
+        }
+}
